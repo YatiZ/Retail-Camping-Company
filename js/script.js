cc6aebff76209ed19d89354b1a9dc14123df49c6
YatiZ <zuzu62113@gmail.com>
2023-11-06 06:57:41 +0630

    final-commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-//navbar active
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("nav-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+//for collapse 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    console.log('hello')
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
   });
 }
 
